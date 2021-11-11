659803beb1825f7f2ee83e79e35aa28c3f4f77a0
Dmitry <lowerf@mail.ru>
2021-11-11 11:45:35 +0300

    Version 11.11.2021

--- Scripts/Architecture_Oracle/p_create_all.sql
+++ Scripts/Architecture_Oracle/p_create_all.sql
@@ -18,43 +18,43 @@ BEGIN
           table_name = UPPER(p_table_name);
 
 
-    EXECUTE IMMEDIATE 'ALTER TABLE '||p_table_name||
-                      ' ADD CONSTRAINT PK_'||p_table_name||' PRIMARY KEY('||v_pk_col_name||')';
-
-    EXECUTE IMMEDIATE 'CREATE SEQUENCE SEQ_'||p_table_name||'
-                MINVALUE 1 
-                START WITH 1
-                INCREMENT BY 1
-                CACHE 20';
-
-    DECLARE 
-        v_constraint_type VARCHAR(20);
-        v_index_name VARCHAR(100);
-        v_pk_column VARCHAR(100);
-    BEGIN
-        SELECT constraint_type, index_name 
-            INTO v_constraint_type, v_index_name
-        FROM user_constraints 
-        WHERE constraint_type = 'P' AND 
-              table_name = p_table_name;
-        
-        SELECT column_name
-            INTO v_pk_column
-        FROM user_ind_columns
-        WHERE index_name = v_index_name;
-    
-        EXECUTE IMMEDIATE 'CREATE OR REPLACE TRIGGER T_B_'||p_table_name||'
-                             BEFORE INSERT 
-                                    ON '||p_table_name||'
-                                    FOR EACH ROW
-                           BEGIN
-                                IF :NEW.'||v_pk_column||' IS NULL THEN 
-                                    SELECT SEQ_'||p_table_name||'.NEXTVAL
-                                        INTO :NEW.'||v_pk_column||'
-                                    FROM dual;
-                                END IF;
-                           END;';
-    END; 
+--    EXECUTE IMMEDIATE 'ALTER TABLE '||p_table_name||
+--                      ' ADD CONSTRAINT PK_'||p_table_name||' PRIMARY KEY('||v_pk_col_name||')';
+--
+--    EXECUTE IMMEDIATE 'CREATE SEQUENCE SEQ_'||p_table_name||'
+--                MINVALUE 1 
+--                START WITH 1
+--                INCREMENT BY 1
+--                CACHE 20';
+--
+--    DECLARE 
+--        v_constraint_type VARCHAR(20);
+--        v_index_name VARCHAR(100);
+--        v_pk_column VARCHAR(100);
+--    BEGIN
+--        SELECT constraint_type, index_name 
+--            INTO v_constraint_type, v_index_name
+--        FROM user_constraints 
+--        WHERE constraint_type = 'P' AND 
+--              table_name = p_table_name;
+--        
+--        SELECT column_name
+--            INTO v_pk_column
+--        FROM user_ind_columns
+--        WHERE index_name = v_index_name;
+--    
+--        EXECUTE IMMEDIATE 'CREATE OR REPLACE TRIGGER T_B_'||p_table_name||'
+--                             BEFORE INSERT 
+--                                    ON '||p_table_name||'
+--                                    FOR EACH ROW
+--                           BEGIN
+--                                IF :NEW.'||v_pk_column||' IS NULL THEN 
+--                                    SELECT SEQ_'||p_table_name||'.NEXTVAL
+--                                        INTO :NEW.'||v_pk_column||'
+--                                    FROM dual;
+--                                END IF;
+--                           END;';
+--    END; 
         
     --Block to generate CRUD package for table
     DECLARE 
@@ -107,7 +107,7 @@ BEGIN
                     IF cur_get_col_name%ROWCOUNT < v_count_col THEN
                         v_create_text_parm:= v_create_text_parm||'P_NEW_'||v_col_name||' IN '||
                                               p_table_name||'.'||v_col_name||'%TYPE,'||chr(10);
-                        v_update_text_parm:= v_create_text_parm||v_four_tab||'P_NEW_'||v_col_name||' IN '||
+                        v_update_text_parm:= v_update_text_parm||v_four_tab||'P_NEW_'||v_col_name||' IN '||
                                               p_table_name||'.'||v_col_name||'%TYPE,'||chr(10);
                     ELSE 
                         v_create_text_parm:= v_create_text_parm||v_four_tab||'P_NEW_'||v_col_name||' IN '||
